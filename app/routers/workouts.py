a069d109ae86a23dd933c58e6c103d87ae850369
Shane <shanestroh22@gmail.com>
2026-02-24 13:07:21 -0700

    Excluded empty values for optional inputs

--- app/routers/workouts.py
+++ app/routers/workouts.py
@@ -7,7 +7,7 @@ workout_storage = []
 
 @router.post("/workouts")
 def create_workout(workout : CreateWorkout):
-    workout_dict = workout.model_dump()
+    workout_dict = workout.model_dump(exclude_none=True)
 
     workout_dict["id"] = len(workout_storage) + 1
     workout_storage.append(workout_dict)
--- app/schemas/workout.py
+++ app/schemas/workout.py
@@ -1,11 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Optional
 from datetime import date
 
 
 class CreateWorkout(BaseModel):
     exercise: str
-    sets: int
-    reps: float
+    sets: Optional[int] = None
+    reps: Optional[float] = None
     weight: Optional[float] = None
     date: date
+    notes : Optional[str] = None
+
+    #Converts SwaggerUI default numeric values "0" to None
+    @field_validator("sets", "reps", "weight", mode = "before")
+    @classmethod
+    def zero_to_none(cls, v):
+        if v == 0 or v == 0.0:
+            return None
+        return v
+
+    #Convert "" or Swagger's default "string" to None
+    @field_validator("notes", mode = "before")
+    @classmethod
+    def clean_notes(cls,v):
+        if v is None:
+            return None
+
+        if isinstance(v, str):
+            v = v.strip()
+            if v == "" or v.lower() == "string":
+                return None
+        return v
