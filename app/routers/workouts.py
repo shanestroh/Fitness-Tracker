46120699e67c326ccd56821f1a8aefe2f9906c5e
Shane <shanestroh22@gmail.com>
2026-02-23 17:28:49 -0700

    add POST and GET workout endpoints with in-memory storage

---
+++ app/routers/workouts.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from app.schemas.workout import CreateWorkout
+
+router = APIRouter()
+
+workout_storage = []
+
+@router.post("/workouts")
+def create_workout(workout : CreateWorkout):
+    workout_dict = workout.model_dump()
+
+    workout_dict["id"] = len(workout_storage) + 1
+    workout_storage.append(workout_dict)
+
+    return workout_dict
+
+@router.get("/workouts")
+def get_workouts():
+    return workout_storage
